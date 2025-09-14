bc219a08d70a9b616df1bd6db9cf2806a67ed954
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-14 21:05:26 +0530

    feat: add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ src/utils.js/api-error.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = 'Something went wrong',
+    errors = [],
+    stack = ''
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.name = this.constructor.name;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor); // Error.captureStackTrace(this, this.constructor) is called to generate a stack trace automatically.
+    }
+  }
+}
+
+export { ApiError };
---
+++ src/utils.js/api-response.js
@@ -0,0 +1,19 @@
+class ApiResponse {
+  constructor(statusCode, data, message = 'Success') {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode >= 200 && statusCode < 300; // success is true for status codes in the range of 200-299
+  }
+
+  send(res) {
+    return res.status(this.statusCode).json({
+      success: this.success,
+      statusCode: this.statusCode,
+      message: this.message,
+      data: this.data,
+    });
+  }
+}
+
+export { ApiResponse };
